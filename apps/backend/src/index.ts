6d14ec25d21dc62f3f7cd82a32743b288489343b
deveshh30 <devesh30gupta@gmail.com>
2026-03-26 01:26:46 +0530

    first commit

---
+++ apps/backend/src/index.ts
@@ -0,0 +1,8 @@
+import { configDotenv } from "dotenv";
+configDotenv();
+
+export const config = {
+    port : process.env.PORT || 4000,
+    mongoUri : process.env.MONGO_URI || '',
+    jwtSecret : process.env.JWT_SECRET || ''
+};
