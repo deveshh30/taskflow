30c43395d0e7c8598d13fb563caf8a313a68b054
deveshh30 <devesh30gupta@gmail.com>
2026-05-09 18:18:13 +0530

    Add rate limiting middleware to authentication routes for enhanced security

--- apps/backend/src/routes/Authh.route.ts
+++ apps/backend/src/routes/Authh.route.ts
@@ -2,17 +2,18 @@ import { Request, Response, Router } from "express";
 import { loginUser, registerUser } from "../controller/auth.controller";
 import { protect } from "../middleware/Auth.middleware";
 import {  createWorkspace } from "../controller/Workspace.controller";
+import { authLimiter } from "../middleware/rateLimit.middleware";
 
 const router = Router();
 
-router.post('/register' , registerUser);
-router.post('/login' , loginUser);
+router.post('/register' , authLimiter ,  registerUser);
+router.post('/login' , authLimiter , loginUser);
 
 router.get('/me', protect, (req, res) => {
   res.json({
     success: true,
     message: 'You are authenticated',
-    userId: (req as any).user?.userId
+    userId: req.user?.userId
   });
 });
 export default router;
